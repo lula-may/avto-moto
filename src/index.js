be628e47bc50b19c7eb1357d5c385b780cac7303
Yulia Elagina <julia-elagina@yandex.ru>
2021-07-21 15:45:27 +0300

    Подключает шрифты

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-
+import './styles/style.scss';
 ReactDOM.render(
   <React.StrictMode>
     <App />
