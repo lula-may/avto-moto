affd66e580133a24bcfcc40a5c8ff42999f29eef
Yulia Elagina <julia-elagina@yandex.ru>
2021-07-24 13:01:17 +0300

    Создает компоненты Шапки и Подвала

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './style.scss';
+
+export default function Footer() {
+  return (
+    <footer className="main-footer">
+      <div className="main-footer__container">
+        <ul className="main-footer__menu">
+          <li className="main-footer__item">
+            <a href="#" className="main-footer__link">Корпоративным клиентам</a>
+          </li>
+          <li className="main-footer__item">
+            <a href="#" className="main-footer__link">Каршеринг</a>
+          </li>
+          <li className="main-footer__item">
+            <a href="#" className="main-footer__link">Клиентам</a>
+          </li>
+          <li className="main-footer__item">
+            <a href="#" className="main-footer__link">Аренда авто</a>
+          </li>
+          <li className="main-footer__item">
+            <a href="#" className="main-footer__link">Как продать авто</a>
+          </li>
+          <li className="main-footer__item">
+            <a href="#" className="main-footer__link">Trade-in</a>
+          </li>
+          <li className="main-footer__item">
+            <a href="#" className="main-footer__link">Test drive</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/header/header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './style.scss';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <div className="header__logo logo">
+          <a href="#" className="logo__link">
+            <img src="./img/logo.png" alt="Логотип Авто-Мото"/>
+          </a>
+        </div>
+        <nav className="header__nav main-nav">
+          <ul className="main-nav__list">
+            <li className="main-nav__item">
+              <a href="#" className="main-nav__link">Автомобили</a>
+            </li>
+            <li className="main-nav__item">
+              <a href="#" className="main-nav__link">Контакты</a>
+            </li>
+            <li className="main-nav__item">
+              <a href="#" className="main-nav__link">Услуги </a>
+            </li>
+            <li className="main-nav__item">
+              <a href="#" className="main-nav__link">Вакансии</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
