3497924c66d0303a6f1d939c95b418b60af93232
Yulia Elagina <julia-elagina@yandex.ru>
2021-07-24 13:14:02 +0300

    Создает компоент Main, подключает в него созданные компоненты

---
+++ src/components/main/main.jsx
@@ -0,0 +1,30 @@
+import React, {Fragment} from 'react';
+import './style.scss';
+
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import Info from '../info/info';
+import Slider from '../slider/slider';
+import Tabs from '../tabs/tabs';
+
+export default function Main() {
+  return (
+    <Fragment>
+      <Header />
+      <main className="page-main">
+        <div className="page-main__container">
+          <article className="auto">
+            <div className="auto__column auto__column--left">
+              <Slider />
+            </div>
+            <div className="auto__column auto__column--right">
+              <Info />
+            </div>
+          </article>
+          <Tabs />
+        </div>
+      </main>
+      <Footer />
+    </Fragment>
+  );
+}
