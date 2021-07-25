f0667dc41632495b2a62c2e69a3c1bcab4681598
Yulia Elagina <julia-elagina@yandex.ru>
2021-07-26 01:20:09 +0300

    Создает константы для табов и утилитарную функцию для получения класса элемента

---
+++ src/const.js
@@ -0,0 +1,13 @@
+const Tab = {
+  PROPERTIES: 'properties',
+  REVIEWS: 'reviews',
+  CONTACTS: 'contacts',
+};
+
+const TabIdToTitle = {
+  properties: 'Характеристики',
+  reviews: 'Отзывы',
+  contacts: 'Контакты',
+};
+
+export {Tab, TabIdToTitle};
