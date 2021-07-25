96cbbf759ca37f581f76255e4b76261c35220c58
Yulia Elagina <julia-elagina@yandex.ru>
2021-07-25 23:07:09 +0300

    Удаляет компонент Main, его содержимое и стили переносит в App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import './style.scss';
 
-import Main from '../main/main';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import Info from '../info/info';
+import Popup from '../popup/popup';
+import Slider from '../slider/slider';
+import Tabs from '../tabs/tabs';
+import {auto} from '../../data/auto';
+
+const {features, images, info} = auto;
+
 function App() {
-  return <Main />;
+  return (
+    <Fragment>
+      <Header />
+      <main className="page-main">
+        <div className="page-main__container">
+          <article className="auto">
+            <div className="auto__column auto__column--left">
+              <Slider images={images} />
+            </div>
+            <div className="auto__column auto__column--right">
+              <Info properties={info} />
+            </div>
+          </article>
+          <Tabs features={features}/>
+        </div>
+      </main>
+      <Footer />
+      <Popup />
+    </Fragment>
+  );
 }
 
 export default App;
