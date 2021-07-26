023dd9781cb389dd4e139fc254d70f6045adc8b8
Yulia Elagina <julia-elagina@yandex.ru>
2021-07-26 19:57:36 +0300

    Реализует функционал добавления нового комментария

--- src/components/review-form/review-form.jsx
+++ src/components/review-form/review-form.jsx
@@ -1,67 +1,139 @@
-import React from 'react';
+import React, {Fragment, useCallback, useState} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 import './style.scss';
+import {ActionCreator} from '../../store/actions';
 
-export default function ReviewForm() {
+const RATINGS = [5, 4, 3, 2, 1];
+
+function ReviewForm(props) {
+  const {onCloseButtonClick, onCommentSubmit} = props;
+  const [userName, setUserName] = useState('');
+  const [advantage, setAdvantage] = useState('');
+  const [disadvantage, setDisadvantage] = useState('');
+  const [text, setText] = useState('');
+  const [rating, setRating] = useState(0);
+
+  const onUserNameChange = useCallback((evt) => {
+    evt.preventDefault();
+    setUserName(evt.target.value);
+  }, []);
+
+  const onAdvantageChange = useCallback((evt) => {
+    evt.preventDefault();
+    setAdvantage(evt.target.value);
+  }, []);
+
+  const onDisadvantageChange = useCallback((evt) => {
+    evt.preventDefault();
+    setDisadvantage(evt.target.value);
+  }, []);
+
+  const onTextChange = useCallback((evt) => {
+    evt.preventDefault();
+    setText(evt.target.value);
+  }, []);
+
+  const onRatingChange = useCallback((evt) => {
+    evt.preventDefault();
+    setRating(evt.target.value);
+  }, []);
+
+  const handleSubmit = useCallback((evt) => {
+    evt.preventDefault();
+    const newComment = {
+      id: Math.round(Math.random() * 1000),
+      user: {
+        id: 0,
+        name: userName,
+      },
+      comment: {
+        advantage,
+        disadvantage,
+        text,
+      },
+      rating: Number(rating),
+    };
+    onCommentSubmit(newComment);
+  }, [advantage, disadvantage, text, rating, onCommentSubmit, userName]);
+
   return (
     <article className="review-form">
       <h1 className="review-form__header">Оставить отзыв</h1>
-      <button className="review-form__close" type="button">Закрыть</button>
-      <form action="#" method="post" id="review-form">
+      <button
+        className="review-form__close"
+        onClick={onCloseButtonClick}
+        type="button"
+      >Закрыть
+      </button>
+      <form action="#" method="post" id="review-form" onSubmit={handleSubmit}>
         <div className="review-form__container">
           <div className="review-form__column">
             <p className="review-form__field">
               <label htmlFor="name" className="visually-hidden">Введите ваше имя</label>
-              <input type="text" id="name" name="user-name" placeholder="Имя" required/>
+              <input
+                type="text"
+                id="name"
+                name="user-name"
+                placeholder="Имя"
+                onChange={onUserNameChange}
+                required
+              />
               <span className="review-form__message">Пожалуйста, заполните поле</span>
             </p>
             <p className="review-form__field">
               <label htmlFor="advantages" className="visually-hidden">Достоинства</label>
-              <input type="text" id="advantages" name="advantages" placeholder="Достоинства"/>
+              <input
+                type="text"
+                id="advantages"
+                name="advantages"
+                onChange={onAdvantageChange}
+                placeholder="Достоинства"
+              />
             </p>
             <p className="review-form__field">
               <label htmlFor="disadvantages" className="visually-hidden">Недостатки</label>
-              <input type="text" id="disadvantages" name="disadvantages" placeholder="Недостатки"/>
+              <input
+                type="text"
+                id="disadvantages"
+                name="disadvantages"
+                onChange={onDisadvantageChange}
+                placeholder="Недостатки"
+              />
             </p>
           </div>
           <div className="review-form__column">
             <div className="review-form__rating">
               <span className="review-form__label">Оцените товар:</span>
               <div className="rating">
-                <input className="visually-hidden" type="radio" name="rating" id="5-stars" value="5" title="Отлично" />
-                <label htmlFor="5-stars">
-                  <svg className="rating__image" width="27" height="25">
-                    <use xlinkHref="#star"></use>
-                  </svg>
-                </label>
-                <input className="visually-hidden" type="radio" name="rating" id="4-stars" value="4" title="Хорошо" />
-                <label htmlFor="4-stars">
-                  <svg className="rating__image" width="27" height="25">
-                    <use xlinkHref="#star"></use>
-                  </svg>
-                </label>
-                <input className="visually-hidden" type="radio" name="rating" id="3-stars" value="3" title="Нормально" />
-                <label htmlFor="3-stars">
-                  <svg className="rating__image" width="27" height="25">
-                    <use xlinkHref="#star"></use>
-                  </svg>
-                </label>
-                <input className="visually-hidden" type="radio" name="rating" id="2-stars" value="2" title="Плохо" />
-                <label htmlFor="2-stars">
-                  <svg className="rating__image" width="27" height="25">
-                    <use xlinkHref="#star"></use>
-                  </svg>
-                </label>
-                <input className="visually-hidden" type="radio" name="rating" id="1-stars" value="1" title="Ужасно" />
-                <label htmlFor="1-stars">
-                  <svg className="rating__image" width="27" height="25">
-                    <use xlinkHref="#star"></use>
-                  </svg>
-                </label>
+                {RATINGS.map((value) => (
+                  <Fragment key={value}>
+                    <input
+                      className="visually-hidden"
+                      id={`${value}-stars`}
+                      name="rating"
+                      onChange={onRatingChange}
+                      type="radio"
+                      value={value}
+                    />
+                    <label htmlFor={`${value}-stars`}>
+                      <svg className="rating__image" width="27" height="25">
+                        <use xlinkHref="#star"></use>
+                      </svg>
+                    </label>
+                  </Fragment>
+                ))}
               </div>
             </div>
             <p className="review-form__field">
               <label htmlFor="comment" className="visually-hidden">Напишите здесь свой комментарий</label>
-              <textarea id="comment" name="comment" placeholder="Комментарий" required/>
+              <textarea
+                id="comment"
+                name="comment"
+                onChange={onTextChange}
+                placeholder="Комментарий"
+                required
+              />
             </p>
           </div>
         </div>
@@ -70,3 +142,19 @@ export default function ReviewForm() {
     </article>
   );
 }
+
+ReviewForm.propTypes = {
+  onCloseButtonClick: PropTypes.func.isRequired,
+  onCommentSubmit: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  onCloseButtonClick: () => dispatch(ActionCreator.closePopup()),
+
+  onCommentSubmit: (comment) => {
+    dispatch(ActionCreator.addComment(comment));
+    dispatch(ActionCreator.closePopup());
+  },
+});
+
+export default connect(null, mapDispatchToProps)(ReviewForm);
